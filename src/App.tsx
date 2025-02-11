a5adfe7f8fd1b05ce84a2a02b658e9f4857ffc67
RencyDuqs <dayneduque22@gmail.com>
2025-02-11 13:06:05 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,55 @@
+import { useState } from 'react';
+import { sculptureList } from './data.js';
+
+export default function Gallery() {
+  const [index, setIndex] = useState(0);
+  const [showMore, setShowMore] = useState(false);
+  const hasNext = index < sculptureList.length - 1;
+  const hasPrevious = index > 0;
+
+  function handleNextClick() {
+    if (hasNext) {
+      setIndex(index + 1);
+    } else {
+      setIndex(0);
+    }
+  }
+
+  function handleBackClick() {
+    if (hasPrevious) {
+      setIndex(index - 1);
+    } else {
+      setIndex(sculptureList.length - 1);  
+    }
+  }
+
+  function handleMoreClick() {
+    setShowMore(!showMore);
+  }
+
+  let sculpture = sculptureList[index];
+  return (
+    <>
+      <h1>Rency Dayne M. Duque</h1> 
+      
+      <button onClick={handleBackClick}>Back</button> 
+      <button onClick={handleNextClick}>Next</button> 
+      
+      <h2>
+        <i>{sculpture.name} </i>
+        - {sculpture.artist}
+      </h2>
+      <h3>
+        ({index + 1} of {sculptureList.length})
+      </h3>
+      <button onClick={handleMoreClick}>
+        {showMore ? 'Hide' : 'Show'} details
+      </button>
+      {showMore && <p>{sculpture.description}</p>}
+      <img
+        src={sculpture.url}
+        alt={sculpture.alt}
+      />
+    </>
+  );
+}
